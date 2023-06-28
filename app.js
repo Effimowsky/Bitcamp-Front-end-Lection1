722030cbb2a8856be30413a0f4d44e9641b2ddc3
Effimowsky <94548214+Effimowsky@users.noreply.github.com>
2023-06-28 22:37:23 +0400

    do homeworks

---
+++ app.js
@@ -0,0 +1,62 @@
+// // დავალება 1
+// console.log("Hello World!")
+
+// // დავალება 2
+// let myNumber = 28
+// console.log(myNumber)
+
+// // დავალება 3
+// let number1 = 45
+// let number2 = 13
+
+// let count = number1 + number2
+// console.log(count)
+
+// // დავალება 4
+// let number3 = 255
+// let number4 = 29
+
+// let result = number3 - number4 
+// console.log(result)
+
+// // დავალება 5 
+// let number1 = 19
+// let number2 = 20
+
+// console.log(number1 * number2)
+
+// // დავალება 6
+// let number3 = 255
+// let number4 = 5
+
+// let result = number3 / number4 
+// console.log(result)
+
+// // დავალება 7
+// let number1 = 10
+// let number2 = 11
+// let reminder = number1 % number2
+
+// console.log(reminder)
+
+// // დავალება 8 
+// let myName = "Omari"
+// console.log(myName.length)
+
+// // დავალება 9 
+// let string1 = "Luka " 
+// let string2 = "Loria"
+
+// console.log(string1 + string2)
+
+// // დავალება 10
+// let myName = "omari"
+
+// console.log(myName.toUpperCase())
+
+// // დავალება 11
+// let myName = "OMARI"
+
+// console.log(myName.toLowerCase())
+
+
